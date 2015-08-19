2cb4871c6fc0d610b9b3ed894301dbff5bac587f
constantinecarter <jjamaican_@hotmail.com>
2015-08-20 09:54:58 +1200

    wwcwec

--- D202/UI_WinForms/Form1.cs
+++ D202/UI_WinForms/Form1.cs
@@ -7,14 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using CL_Application;
 
 namespace UI_WinForms
 {
     public partial class Form1 : Form
     {
         public Form1()
-        {//ertyetryrt..hjuybgujg
+        {//tester
             InitializeComponent();
         }
     }
