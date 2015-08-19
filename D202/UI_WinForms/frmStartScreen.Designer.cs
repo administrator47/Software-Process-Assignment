488cfe5d806c254e6dfd9ed6fb14cf7cc191ee84
constantinecarter <jjamaican_@hotmail.com>
2015-08-20 11:21:02 +1200

    3453

--- D202/UI_WinForms/frmStartScreen.Designer.cs
+++ D202/UI_WinForms/frmStartScreen.Designer.cs
@@ -31,11 +31,23 @@
             this.btnLoadCategories = new System.Windows.Forms.Button();
             this.lbxLoad = new System.Windows.Forms.ListBox();
             this.btnLoadAll = new System.Windows.Forms.Button();
+            this.gbxLoad = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.gbxLoad.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnLoadCategories
             // 
-            this.btnLoadCategories.Location = new System.Drawing.Point(12, 33);
+            this.btnLoadCategories.Location = new System.Drawing.Point(20, 34);
             this.btnLoadCategories.Name = "btnLoadCategories";
             this.btnLoadCategories.Size = new System.Drawing.Size(106, 23);
             this.btnLoadCategories.TabIndex = 0;
@@ -45,30 +57,127 @@
             // lbxLoad
             // 
             this.lbxLoad.FormattingEnabled = true;
-            this.lbxLoad.Location = new System.Drawing.Point(25, 76);
+            this.lbxLoad.Location = new System.Drawing.Point(20, 102);
             this.lbxLoad.Name = "lbxLoad";
             this.lbxLoad.Size = new System.Drawing.Size(171, 160);
             this.lbxLoad.TabIndex = 1;
             // 
             // btnLoadAll
             // 
-            this.btnLoadAll.Location = new System.Drawing.Point(124, 33);
+            this.btnLoadAll.Location = new System.Drawing.Point(148, 34);
             this.btnLoadAll.Name = "btnLoadAll";
             this.btnLoadAll.Size = new System.Drawing.Size(95, 23);
             this.btnLoadAll.TabIndex = 2;
             this.btnLoadAll.Text = "Load all Papers";
             this.btnLoadAll.UseVisualStyleBackColor = true;
             // 
+            // gbxLoad
+            // 
+            this.gbxLoad.BackColor = System.Drawing.Color.SeaGreen;
+            this.gbxLoad.Controls.Add(this.lbxLoad);
+            this.gbxLoad.Controls.Add(this.btnLoadCategories);
+            this.gbxLoad.Controls.Add(this.btnLoadAll);
+            this.gbxLoad.Location = new System.Drawing.Point(24, 78);
+            this.gbxLoad.Name = "gbxLoad";
+            this.gbxLoad.Size = new System.Drawing.Size(270, 298);
+            this.gbxLoad.TabIndex = 3;
+            this.gbxLoad.TabStop = false;
+            this.gbxLoad.Enter += new System.EventHandler(this.gbxLoad_Enter);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.HotPink;
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(355, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(269, 298);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(36, 103);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(171, 160);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(147, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load all Papers";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Load Categories";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.groupBox2.Controls.Add(this.listBox3);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Location = new System.Drawing.Point(708, 78);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(269, 298);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(36, 103);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(171, 160);
+            this.listBox3.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(19, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 23);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "Load Categories";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(147, 35);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Load all Papers";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
             // frmStartScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(446, 284);
-            this.Controls.Add(this.btnLoadAll);
-            this.Controls.Add(this.lbxLoad);
-            this.Controls.Add(this.btnLoadCategories);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::UI_WinForms.Properties.Resources.pic;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1190, 636);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gbxLoad);
             this.Name = "frmStartScreen";
             this.Text = "BICT Course Information";
+            this.gbxLoad.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -78,6 +187,15 @@
         private System.Windows.Forms.Button btnLoadCategories;
         private System.Windows.Forms.ListBox lbxLoad;
         private System.Windows.Forms.Button btnLoadAll;
+        private System.Windows.Forms.GroupBox gbxLoad;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
