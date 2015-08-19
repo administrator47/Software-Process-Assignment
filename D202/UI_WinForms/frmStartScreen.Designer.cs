2d8d8a55611628325e105d79db9fc4838fe94ede
michiepayne <michiepayne@gmail.com>
2015-08-20 10:42:08 +1200

    pie

--- D202/UI_WinForms/frmStartScreen.Designer.cs
+++ D202/UI_WinForms/frmStartScreen.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmStartScreen));
             this.btnLoadCategories = new System.Windows.Forms.Button();
             this.lbxLoad = new System.Windows.Forms.ListBox();
             this.btnLoadAll = new System.Windows.Forms.Button();
@@ -35,7 +36,7 @@
             // 
             // btnLoadCategories
             // 
-            this.btnLoadCategories.Location = new System.Drawing.Point(12, 33);
+            this.btnLoadCategories.Location = new System.Drawing.Point(12, 84);
             this.btnLoadCategories.Name = "btnLoadCategories";
             this.btnLoadCategories.Size = new System.Drawing.Size(106, 23);
             this.btnLoadCategories.TabIndex = 0;
@@ -45,14 +46,14 @@
             // lbxLoad
             // 
             this.lbxLoad.FormattingEnabled = true;
-            this.lbxLoad.Location = new System.Drawing.Point(25, 76);
+            this.lbxLoad.Location = new System.Drawing.Point(33, 125);
             this.lbxLoad.Name = "lbxLoad";
             this.lbxLoad.Size = new System.Drawing.Size(171, 160);
             this.lbxLoad.TabIndex = 1;
             // 
             // btnLoadAll
             // 
-            this.btnLoadAll.Location = new System.Drawing.Point(124, 33);
+            this.btnLoadAll.Location = new System.Drawing.Point(124, 84);
             this.btnLoadAll.Name = "btnLoadAll";
             this.btnLoadAll.Size = new System.Drawing.Size(95, 23);
             this.btnLoadAll.TabIndex = 2;
@@ -63,7 +64,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(446, 284);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(796, 435);
             this.Controls.Add(this.btnLoadAll);
             this.Controls.Add(this.lbxLoad);
             this.Controls.Add(this.btnLoadCategories);
