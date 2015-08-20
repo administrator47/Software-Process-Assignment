1a4822636a491e442c2839dea4d2cf1f8de35800
michiepayne <michiepayne@gmail.com>
2015-08-20 12:04:42 +1200

    form

--- D202/UI_WinForms/frmStartScreen.cs
+++ D202/UI_WinForms/frmStartScreen.cs
@@ -26,5 +26,75 @@ namespace UI_WinForms
         {
 
         }
+
+        private void gbxPaperInfo_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPaperPrerequisite_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSemester_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSemester_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxPaperPrerequisite_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxCategory_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblCategory_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gbxLecturer_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxYear_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxDescription_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDescription_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbxPaperName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblPaperName_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
