e65cf267ca36d999e7430548b00d8cefbeb7e666
techgeeknz <git@tech.geek.nz>
2015-08-27 10:33:04 +1200

    Add Application.cs class

---
+++ D202/UI_WinForms/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_WinForms.Classes
+{
+    class Application
+    {
+        public List<Category> LoadCategories();
+
+        public List<Course>
+    }
+}
