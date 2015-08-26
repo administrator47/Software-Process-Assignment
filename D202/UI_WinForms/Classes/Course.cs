de113e41a364eeccdd21959f86f6cead19929294
techgeeknz <git@tech.geek.nz>
2015-08-27 11:56:00 +1200

    Add 'compulsory' field to Course

--- D202/UI_WinForms/Classes/Course.cs
+++ D202/UI_WinForms/Classes/Course.cs
@@ -9,13 +9,14 @@ namespace UI_WinForms.Classes
 {
     class Course
     {
-        public Course(int id, string name, Category category, Course prerequisite, Lecturer lecturer)
+        public Course(int id, string name, Category category, Course prerequisite, Lecturer lecturer, bool compulsory)
         {
             ID = id;
             Name = name;
             CourseCategory = category;
             Prerequisite = prerequisite;
             CourseLecturer = lecturer;
+            Compulsory = compulsory;
         }
 
         public int ID { get; set; }
@@ -27,5 +28,7 @@ namespace UI_WinForms.Classes
         public Course Prerequisite { get; set; }
 
         public Lecturer CourseLecturer { get; set; }
+
+        public bool Compulsory { get; set; }
     }
 }
