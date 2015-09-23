ea7aaac464cd50e01b07678edd2462bc0c9f2508
constantinecarter <jjamaican_@hotmail.com>
2015-09-24 09:41:15 +1200

    something21

--- D202/UI_WinForms/frmPlanOfStudy.cs
+++ D202/UI_WinForms/frmPlanOfStudy.cs
@@ -34,7 +34,7 @@ namespace UI_WinForms
 
         private void btnRefresh_Click(object sender, EventArgs e)
         {
-
+            SetupCourses();
         }
 
         private void SetupCourses ()
@@ -50,15 +50,24 @@ namespace UI_WinForms
                 year = 2;
             }
 
-            // Array of course objects retrieved from database based on year and semester
-            Course[] courseList = Course.LoadIDs(year,semester);
+            // list of course objects retrieved from database based on year and semester
+            List<Course> courseList = new List<Course>();
+            courseList.AddRange(Course.LoadIDs(year, semester));
+            courseList.AddRange(Course.LoadIDs(year, 3));
+
+            // Any duplicates are filtered out
+            //courseList = courseList.GroupBy(x => x)
+            // .Where(x => x.Count() > 1)
+            // .Select(x => x.Key)
+            // .ToList();
 
             // All compulsory Course objects are loaded into a new course array
             Course[] compulsory = (from comp in courseList
                                    where comp.Compulsory == true
                                    select comp).ToArray();
 
-            // Course array is added to the listbox
+            // Listbox is cleared then course array is added
+            lbxCompulsory.Items.Clear();
             lbxCompulsory.Items.AddRange(compulsory);
 
             // All non-compulsory Course objects are loaded into a new course array
@@ -67,9 +76,17 @@ namespace UI_WinForms
                                    select comp).ToArray();
 
             // Course array is added to the listbox
+            cbxOne.Items.Clear();
             cbxOne.Items.AddRange(nonCompulsory);
+            cbxTwo.Items.Clear();
             cbxTwo.Items.AddRange(nonCompulsory);
+            cbxThree.Items.Clear();
             cbxThree.Items.AddRange(nonCompulsory);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
         }
 
 
